de72ecf141c14ffa03480706016ca1dbe5612271
telepok <atelepnev@gmail.com>
2017-08-25 16:01:11 +0700

    1

--- tmp.php
+++ tmp.php
@@ -6,6 +6,17 @@
  * Time: 0:52
  */
 
-phpinfo()
+$user = "root";
+$password = "Passw0rd";
 
-?>
+//$dbh = new PDO('mysql:host=localhost;dbname=sakila', $root, $password);
+
+//var_dump($dbh->query('select * from actor'));
+
+$dbh = new PDO('mysql:host=localhost;dbname=sakila', $user, $password);
+foreach($dbh->query('SELECT * from actor') as $row) {
+//    print_r($row);
+    echo "\n";
+}
+
+var_dump($dbh->query('SELECT * from actor'));
