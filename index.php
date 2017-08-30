97c3b54fd055690ca4db247cd35b3aa724b06b63
telepok <atelepnev@gmail.com>
2017-08-30 09:04:00 +0700

    1

--- index.php
+++ index.php
@@ -11,23 +11,49 @@
 </head>
 <body>
 <div class="container">
-<h1>Список студентов</h1>
-<table class="table">
-    <tr>
-        <th>Имя</th>
-        <th>Фамилия</th>
-        <th>Номер группы</th>
-        <th>Баллов</th>
-    </tr>
-    <?php for ($i = 0; $i < 5; $i++): ?>
+    <h1>Список студентов</h1>
+    <form action="">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                   placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">
+                <input type="checkbox" class="form-check-input">
+                Check me out
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <table class="table table-bordered table-striped">
+        <thead class="thead-default">
         <tr>
-            <td><?= $i + 1 ?></td>
-            <td>1</td>
-            <td>1</td>
-            <td>1</td>
+            <th>Имя</th>
+            <th>Фамилия</th>
+            <th>Номер группы</th>
+            <th>Баллов</th>
         </tr>
-    <?php endfor; ?>
-</table>
+        </thead>
+        <tbody>
+        <?php for ($i = 0; $i < 5; $i++): ?>
+            <tr>
+                <td><?= $i + 1 ?></td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        <?php endfor; ?>
+        </tbody>
+    </table>
+    <!--    <div class="d-flex p-2">I'm a flexbox container!</div>-->
 </div>
+
+
 </body>
 </html>
--- tmp.php
+++ tmp.php
@@ -9,14 +9,10 @@
 $user = "root";
 $password = "Passw0rd";
 
-//$dbh = new PDO('mysql:host=localhost;dbname=sakila', $root, $password);
-
-//var_dump($dbh->query('select * from actor'));
-
 $dbh = new PDO('mysql:host=localhost;dbname=sakila', $user, $password);
 foreach($dbh->query('SELECT * from actor') as $row) {
-//    print_r($row);
-    echo "\n";
+    print_r($row[1]);
+    echo "<br>";
 }
 
-var_dump($dbh->query('SELECT * from actor'));
+//var_dump($dbh->query('SELECT * from actor'));
